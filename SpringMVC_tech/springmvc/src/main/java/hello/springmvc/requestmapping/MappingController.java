a88d61249e4843fe63a2c040773495b2e2786ce5
KIMHUEMANG <piyrw9754@gmail.com>
2022-10-18 17:17:32 +0900

    RequestMapping 사용

---
+++ SpringMVC_tech/springmvc/src/main/java/hello/springmvc/requestmapping/MappingController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.requestmapping;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 기본 요청
+     * 둘다 허용 /hello-basic, /hello-basic/
+     * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+
+    @RequestMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+}
