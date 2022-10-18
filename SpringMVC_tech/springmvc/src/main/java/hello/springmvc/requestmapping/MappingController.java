c3e151781cc162779766fdc029e22a6b8b09962d
KIMHUEMANG <piyrw9754@gmail.com>
2022-10-18 17:23:07 +0900

    다양한 Mapping과 PathVariable

--- SpringMVC_tech/springmvc/src/main/java/hello/springmvc/requestmapping/MappingController.java
+++ SpringMVC_tech/springmvc/src/main/java/hello/springmvc/requestmapping/MappingController.java
@@ -3,8 +3,7 @@ package hello.springmvc.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MappingController {
@@ -22,5 +21,33 @@ public class MappingController {
         log.info("helloBasic");
         return "ok";
     }
+
+    @RequestMapping(value = "/mapping-get-v1" , method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션 (코드보기)
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+
+    @GetMapping("/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mappingGetV2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable String userId) {
+        log.info("mappingPath userId = {}", userId);
+        return "ok";
+    }
+
 
 }
