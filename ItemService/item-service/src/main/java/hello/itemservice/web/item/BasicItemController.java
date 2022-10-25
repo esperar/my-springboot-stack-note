8396414473b340b482b6de0d83f1257af5874602
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-25 23:01:51 +0900

    thymeleaf 적용

--- ItemService/item-service/src/main/java/hello/itemservice/web/item/BasicItemController.java
+++ ItemService/item-service/src/main/java/hello/itemservice/web/item/BasicItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.item;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +14,10 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/basic/items")
+@RequiredArgsConstructor
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
-
-    public BasicItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
 
     @GetMapping
     public String items(Model model) {
