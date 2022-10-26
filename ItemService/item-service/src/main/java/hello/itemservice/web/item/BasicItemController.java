74d6852a69d1123cde67462afdf7c4985fa7dc40
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-26 14:09:10 +0900

    @ModelAttribute 상품 등록

--- ItemService/item-service/src/main/java/hello/itemservice/web/item/BasicItemController.java
+++ ItemService/item-service/src/main/java/hello/itemservice/web/item/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,14 +33,51 @@ public class BasicItemController {
     }
 
     @GetMapping("/add")
-    public String addItem() {
+    public String addForm() {
         return "basic/addForm";
     }
 
+   /* @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }*/
+
+
+    /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동 추가
+     */
+    /*@PostMapping
+    public String addItem(@ModelAttribute("item") Item item , Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+        return "basic/item";
+    }*/
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가 */
     @PostMapping("/add")
-    public String save() {
-        return "xxx";
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
     }
+
+
 
     /**
      * 테스트용 데이터 추가
