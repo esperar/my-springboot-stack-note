60caef5996dcfb88002967099bf9b75a5e22ad51
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-29 23:51:54 +0900

    operation 연산

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -82,6 +82,13 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
     @Data
     static class User {
         private String username;
