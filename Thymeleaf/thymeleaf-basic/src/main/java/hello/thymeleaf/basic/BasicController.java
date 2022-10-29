103ab3babbd37da0c0bcc38c76bccc6265a9db83
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-29 23:39:07 +0900

    thymleaf literal

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -76,6 +76,12 @@ public class BasicController {
         return "basic/link";
     }
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data" , "Spring!");
+        return "basic/literal";
+    }
+
     @Data
     static class User {
         private String username;
