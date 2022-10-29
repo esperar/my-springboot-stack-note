d498bb2cf422716bc3bcdce118f6e5bafb8c6a02
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-29 22:17:47 +0900

    thymeleaf url

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -69,6 +69,13 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1" , "data1");
+        model.addAttribute("param2" , "data2");
+        return "basic/link";
+    }
+
     @Data
     static class User {
         private String username;
