2a0b3d0e934a3abccaadc0ebac0349e40edfc387
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-30 13:16:53 +0900

    attribute

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -89,6 +89,11 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
     @Data
     static class User {
         private String username;
