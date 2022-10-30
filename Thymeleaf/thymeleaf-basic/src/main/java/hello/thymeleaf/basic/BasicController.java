d33592060f88af5d25b727f671176e05209a20d7
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-30 13:49:33 +0900

    th:block

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -112,6 +112,12 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
 
