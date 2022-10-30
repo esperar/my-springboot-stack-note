259a04e1234d9bf82e259676b4b3c51127b8481f
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-30 13:44:18 +0900

    thymeleaf comments

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -106,6 +106,12 @@ public class BasicController {
         return "basic/condition";
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data" , "Spring!");
+        return "basic/comments";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
 
