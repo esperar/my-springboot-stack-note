d66a2faebfcf9959c1b6aba17ea91939c0a071e3
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-30 13:29:33 +0900

    thymeleaf each

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -94,6 +94,22 @@ public class BasicController {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+
+        list.add(new User("UserA" , 10));
+        list.add(new User("UserB" , 20));
+        list.add(new User("UserC" , 30));
+
+        model.addAttribute("users",list);
+    }
+
     @Data
     static class User {
         private String username;
