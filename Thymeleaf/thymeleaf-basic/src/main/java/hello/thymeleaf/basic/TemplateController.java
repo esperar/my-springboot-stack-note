2a4fe2d817bb8d7c6dd3907cab5f60ef7fe6dc94
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-30 18:23:31 +0900

    template layout2

--- Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ Thymeleaf/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -18,4 +18,9 @@ public class TemplateController {
     public String layout() {
         return "template/layout/layoutMain";
     }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
 }
