f1630802e37e0456a099675bb4a7857acbee9888
KimHuiMang <piyrw9754@gmail.com>
2022-11-01 20:58:51 +0900

    ItemType DeliveryCode

--- ItemForm/form/src/main/java/hello/itemservice/domain/item/Item.java
+++ ItemForm/form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,8 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
@@ -9,6 +11,11 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private Boolean open; // 판매 여부
+    private List<String> regions; // 등록 지역
+    private ItemType itemType; // 상품 종류
+    private String deliveryCode; // 배송방식
 
     public Item() {
     }
