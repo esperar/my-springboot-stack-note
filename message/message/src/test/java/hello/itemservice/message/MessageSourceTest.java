6e23dac2493aa6b075e8a61322a0cd4213777e90
KimHuiMang <piyrw9754@gmail.com>
2022-11-03 20:16:20 +0900

    메시지와 국제화 사용

--- message/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ message/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
+import java.util.Locale;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -30,5 +32,18 @@ public class MessageSourceTest {
     @Test
     void notFoundMessageCodeDefaultMessage() {
         String result = ms.getMessage("no_code", null, "기본 메시지", null);
-        Assertions.assertThat(result).isEqualTo("기본 메시지"); }
+        Assertions.assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        String argumentMessage = ms.getMessage("안녕", new Object[]{"Spring"}, null);
+        Assertions.assertThat(argumentMessage).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang() {
+        Assertions.assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        Assertions.assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
 }
