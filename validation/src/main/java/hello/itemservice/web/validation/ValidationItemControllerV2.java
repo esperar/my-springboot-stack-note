5a732b84ec078dd7cbb93cef459b29f1f38ade7f
KIMHUEMANG <piyrw9754@gmail.com>
2022-11-08 14:04:35 +0900

    reject, rejectValue 사용

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -116,6 +116,39 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName" , "required" , null);
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price" , "range" , new Object[]{1000,1000000} , null);
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.rejectValue("quantity" , "max" , new Object[]{9999} , null);
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin" ,new Object[]{10000, resultPrice} , null);
+            }
+        }
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={} " , bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
         // 검증 로직
         if(!StringUtils.hasText(item.getItemName())) {
