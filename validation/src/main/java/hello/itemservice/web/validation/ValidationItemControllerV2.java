73ba92cb7b75344ce2c20e48591605fee96f11a6
KIMHUEMANG <piyrw9754@gmail.com>
2022-11-08 21:13:31 +0900

    Validator 검증 로직 분리

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -24,6 +24,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -115,7 +116,42 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+
+
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item" , "itemName" , item.getItemName(), false , new String[]{"required.item.itemName"} , null,null));
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item" , "price" , item.getPrice(), false , new String[]{"range.item.price"} , new Object[]{1000,1000000}, null));
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item" , "quantity" , item.getQuantity(), false , new String[]{"max.item.quantity"}, new Object[]{9999} ,null));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item" , new String[]{"totalPriceMin"} , new Object[]{10000 , resultPrice},null));
+            }
+        }
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={} " , bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    // @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
         // 검증 로직
         if(!StringUtils.hasText(item.getItemName())) {
@@ -149,27 +185,9 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
-        // 검증 로직
-        if(!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(new FieldError("item" , "itemName" , item.getItemName(), false , new String[]{"required.item.itemName"} , null,null));
-        }
-
-        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.addError(new FieldError("item" , "price" , item.getPrice(), false , new String[]{"range.item.price"} , new Object[]{1000,1000000}, null));
-        }
-
-        if(item.getQuantity() == null || item.getQuantity() >= 9999) {
-            bindingResult.addError(new FieldError("item" , "quantity" , item.getQuantity(), false , new String[]{"max.item.quantity"}, new Object[]{9999} ,null));
-        }
-
-        // 특정 필드가 아닌 복합 룰 검증
-        if(item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
-            if(resultPrice < 10000) {
-                bindingResult.addError(new ObjectError("item" , new String[]{"totalPriceMin"} , new Object[]{10000 , resultPrice},null));
-            }
-        }
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
+        itemValidator.validate(item , bindingResult);
 
         if(bindingResult.hasErrors()) {
             log.info("errors={} " , bindingResult);
