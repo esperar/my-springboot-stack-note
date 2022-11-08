783bd54d5cc36fbb6fd64f64bb4b198d043d094b
KIMHUEMANG <piyrw9754@gmail.com>
2022-11-08 12:27:48 +0900

    오류 메시지 처리 1

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -81,7 +81,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
         // 검증 로직
         if(!StringUtils.hasText(item.getItemName())) {
@@ -106,6 +106,40 @@ public class ValidationItemControllerV2 {
 
         if(bindingResult.hasErrors()) {
             log.info("errors={}" , bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
+        // 검증 로직
+        if(!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item" , "itemName" , item.getItemName(), false , new String[]{"required.item.itemName"} , null,null));
+        }
+
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item" , "price" , item.getPrice(), false , new String[]{"range.item.price"} , new Object[]{1000,1000000}, null));
+        }
+
+        if(item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item" , "quantity" , item.getQuantity(), false , new String[]{"max.item.quantity"}, new Object[]{9999} ,null));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item" , new String[]{"totalPriceMin"} , new Object[]{10000 , resultPrice},null));
+            }
+        }
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={} " , bindingResult);
             return "validation/v2/addForm";
         }
 
