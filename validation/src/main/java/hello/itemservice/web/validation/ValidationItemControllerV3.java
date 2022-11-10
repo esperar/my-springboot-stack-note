69659897c0eeafbe30e1bb9ec36bfc0e26fbf679
KimHuiMang <piyrw9754@gmail.com>
2022-11-10 23:19:31 +0900

    검증 로직 추가

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -42,6 +42,16 @@ public class ValidationItemControllerV3 {
     @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult
             bindingResult, RedirectAttributes redirectAttributes) {
+
+        //특정 필드 예외가 아닌 전체 예외
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000,
+                        resultPrice}, null);
+            }
+        }
+
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v3/addForm";
