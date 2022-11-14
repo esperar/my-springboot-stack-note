2cdaa63e3d6856a8aedc4aa30294140c816f4e60
KimHuiMang <piyrw9754@gmail.com>
2022-11-14 20:12:07 +0900

    전송 객체 분리

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,18 +12,18 @@ import javax.validation.constraints.NotNull;
 @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
-    @NotNull(groups = {UpdateCheck.class})
+    // @NotNull(groups = {UpdateCheck.class})
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    // @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000 , max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000 , max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(value = 9999 , groups = {SaveCheck.class})
+//    @NotNull
+//    @Max(value = 9999 , groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
