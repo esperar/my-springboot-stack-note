45f94fe0ba9646a76c9f1165a4d0ed9c1bf33c26
KimHuiMang <piyrw9754@gmail.com>
2022-11-19 22:30:05 +0900

    로그인 관련 로직 작성

---
+++ loginForm/login/src/main/java/hello/login/web/login/LoginController.java
@@ -0,0 +1,43 @@
+package hello.login.web.login;
+
+import hello.login.domain.login.LoginService;
+import hello.login.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+        return "login/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        log.info("login? {}", loginMember);
+
+        if(loginMember == null) {
+            bindingResult.reject("loginFail" , "아이디 또는 비밀번호가 일치하지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공처리 TODO
+        return "redirect:/";
+    }
+}
