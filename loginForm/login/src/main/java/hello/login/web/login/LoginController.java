599b25da9c404e1a326d0651dffe4a4ac7749b8a
KimHuiMang <piyrw9754@gmail.com>
2022-11-20 16:10:14 +0900

    logout 기능 구현

--- loginForm/login/src/main/java/hello/login/web/login/LoginController.java
+++ loginForm/login/src/main/java/hello/login/web/login/LoginController.java
@@ -47,4 +47,16 @@ public class LoginController {
         response.addCookie(idCookie);
         return "redirect:/";
     }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
 }
