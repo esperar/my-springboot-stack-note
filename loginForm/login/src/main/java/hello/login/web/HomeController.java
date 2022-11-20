b6720f80422b58cd7a17e7c67b55958b6e3db1c1
KimHuiMang <piyrw9754@gmail.com>
2022-11-20 18:15:16 +0900

    Http Session 사용

--- loginForm/login/src/main/java/hello/login/web/HomeController.java
+++ loginForm/login/src/main/java/hello/login/web/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -41,7 +42,6 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
         // 세션 관리자에서 저장된 회원 정보 조회
         Object member = sessionManager.getSession(request);
@@ -51,6 +51,24 @@ public class HomeController {
 
         // 로그인
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+        //세션이 없으면 home
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
