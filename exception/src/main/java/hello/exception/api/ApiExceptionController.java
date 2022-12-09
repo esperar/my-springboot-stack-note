95166944949998084fc701a0565e0ceb20e71e07
KimHuiMang <piyrw9754@gmail.com>
2022-12-09 21:38:08 +0900

    HandlerExceptionResolver 적용

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -17,6 +17,10 @@ public class ApiExceptionController {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
         }
+
+        if(id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
         return new MemberDto(id, "hello " + id);
     }
 
