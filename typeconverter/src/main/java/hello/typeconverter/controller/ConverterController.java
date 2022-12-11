ca2678e85139c5792644b227bf4563a607f86d3d
KimHuiMang <piyrw9754@gmail.com>
2022-12-11 16:06:26 +0900

    converter view 적용

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -0,0 +1,18 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter-view";
+    }
+}
