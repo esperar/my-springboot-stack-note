9ecda5f1c849e3f6949b318ed526cb452302d817
KimHuiMang <piyrw9754@gmail.com>
2022-12-11 12:36:38 +0900

    Type Converter Int -> String 구현

---
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package hello.typeconverter.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+}
