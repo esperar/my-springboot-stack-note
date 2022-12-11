1910a39052742110f2f131ae0648b79f589f4d17
KimHuiMang <piyrw9754@gmail.com>
2022-12-11 12:40:43 +0900

    IpPortConverter test 코드 작성

--- typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.type.IpPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ public class ConverterTest {
     void stringToInteger() {
         StringToIntegerConverter converter = new StringToIntegerConverter();
         Integer result = converter.convert("10");
+
         assertThat(result).isEqualTo(10);
     }
 
@@ -18,6 +20,25 @@ public class ConverterTest {
     void integerToString() {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(10);
+
         assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    void ipPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+
+        assertThat(result).isEqualTo("127.0.0.1:8080");
     }
 }
