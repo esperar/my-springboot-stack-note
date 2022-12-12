c8c4ebdbf7f281b4c1778177e64ff52312b3caaa
KimHuiMang <piyrw9754@gmail.com>
2022-12-13 08:07:39 +0900

    FormtterConversionService

---
+++ typeconverter/src/test/java/hello/typeconverter/converter/FormatterConversionServiceTest.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.formatter.MyNumberFormatter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FormatterConversionServiceTest {
+
+    @Test
+    void formattingConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        conversionService.addConverter(new IpPortToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+    }
+}
